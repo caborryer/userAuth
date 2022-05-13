c248589d78de344c86b2bf7f897a57714ad6cb55
yandreac <andrea.corrales@getpulpo.com>
2022-05-13 17:31:20 -0500

    products CRUD
    added a products CRUD

---
+++ src/products/products.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { BrandsService } from './services/brands.service';
+import { BrandsController } from './controllers/brands.controller';
+import { ProductsService } from './services/products.service';
+import { CategoriesService } from './services/categories.service';
+import { ProductsController } from './controllers/products.controller';
+import { CategoriesController } from './controllers/categories.controller';
+
+@Module({
+  providers: [BrandsService, ProductsService, CategoriesService],
+  controllers: [BrandsController, ProductsController, CategoriesController]
+})
+export class ProductsModule {}
